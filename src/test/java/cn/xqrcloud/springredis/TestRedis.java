38fb30c9c64259f178d7f90bea4179d91e20f919
xuqr <1163990163@qq.com>
2020-07-15 17:03:43 +0800

    #Sentinel 修改

---
+++ src/test/java/cn/xqrcloud/springredis/TestRedis.java
@@ -0,0 +1,22 @@
+package cn.xqrcloud.springredis;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.redis.connection.RedisConnection;
+
+/**
+ * 🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌道阻且长，行则将至🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌
+ * 🍁 Program: springredis
+ * 🍁 Description
+ * 🍁 Author: Stephen
+ * 🍁 Create: 2020-07-15 15:58
+ * 🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌行而不辍，未来可期🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌🐌
+ **/
+public class TestRedis {
+    @Test
+    public void testSetGet() throws Exception {
+
+
+    }
+}
